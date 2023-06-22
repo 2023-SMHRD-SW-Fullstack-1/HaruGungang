985361ef10c9362c8d581f19112a4b66bbdf1e19
sin6338ki <jyshin38@gmail.com>
2023-06-22 19:14:07 +0900

    merge shinyoung

--- templete/src/components/Main.jsx
+++ templete/src/components/Main.jsx
@@ -69,181 +69,28 @@ const Main = () => {
    }
 
    const loadData = ()=>{
+
+    let data = {
+        gender : gender,
+        age_range : age
+    }
+
        console.log('loadData');
-       axios.get("http://localhost:8085/haru/main")
+       axios.post("http://localhost:8085/haru/main", data)
        .then((res)=>{
-       
-           //성별, 연령 조건에 따른 jsaon 객체 선택
-           if(gender=='w' && age == 10){
 
-               let temp = [{
-                   Nutriname: `${res.data[0].recNutri.nutri_name}`,
-                   value: parseInt(`${res.data[0].recNutri.click_vol}`)
-               },{
-                   Nutriname: `${res.data[1].recNutri.nutri_name}`,
-                   value: parseInt(`${res.data[1].recNutri.click_vol}`)
-               },{
-                   Nutriname: `${res.data[2].recNutri.nutri_name}`,
-                   value: parseInt(`${res.data[2].recNutri.click_vol}`)
-               }]
-               setRecommend(temp);
-               setVisibleChart(true)
-           
-              }else if(gender=='w' && age == 20){
-
-                let temp = [{
-                  Nutriname: `${res.data[3].recNutri.nutri_name}`,
-                  value: parseInt(`${res.data[3].recNutri.click_vol}`)
-              },{
-                  Nutriname: `${res.data[4].recNutri.nutri_name}`,
-                  value: parseInt(`${res.data[4].recNutri.click_vol}`)
-              },{
-                  Nutriname: `${res.data[5].recNutri.nutri_name}`,
-                  value: parseInt(`${res.data[5].recNutri.click_vol}`)
-              }]
-              setRecommend(temp);
-              setVisibleChart(true)
-
-           }else if(gender=='w' && age == 30){
-            let temp = [{
-              Nutriname: `${res.data[6].recNutri.nutri_name}`,
-              value: parseInt(`${res.data[6].recNutri.click_vol}`)
-          },{
-              Nutriname: `${res.data[7].recNutri.nutri_name}`,
-              value: parseInt(`${res.data[7].recNutri.click_vol}`)
-          },{
-              Nutriname: `${res.data[8].recNutri.nutri_name}`,
-              value: parseInt(`${res.data[8].recNutri.click_vol}`)
-          }]
-          setRecommend(temp);
-          setVisibleChart(true)
-
-           }else if(gender=='w' && age == 40){
-            let temp = [{
-              Nutriname: `${res.data[9].recNutri.nutri_name}`,
-              value: parseInt(`${res.data[9].recNutri.click_vol}`)
-          },{
-              Nutriname: `${res.data[10].recNutri.nutri_name}`,
-              value: parseInt(`${res.data[10].recNutri.click_vol}`)
-          },{
-              Nutriname: `${res.data[11].recNutri.nutri_name}`,
-              value: parseInt(`${res.data[11].recNutri.click_vol}`)
-          }]
-          setRecommend(temp);
-          setVisibleChart(true)
-
-           }else if(gender=='w' && age == 50){
-            let temp = [{
-            Nutriname: `${res.data[12].recNutri.nutri_name}`,
-            value: parseInt(`${res.data[12].recNutri.click_vol}`)
+        let temp = [{
+            Nutriname: `${res.data[0].recNutri.nutri_name}`,
+            value: parseInt(`${res.data[0].recNutri.click_vol}`)
         },{
-            Nutriname: `${res.data[13].recNutri.nutri_name}`,
-            value: parseInt(`${res.data[13].recNutri.click_vol}`)
+            Nutriname: `${res.data[1].recNutri.nutri_name}`,
+            value: parseInt(`${res.data[1].recNutri.click_vol}`)
         },{
-            Nutriname: `${res.data[14].recNutri.nutri_name}`,
-            value: parseInt(`${res.data[14].recNutri.click_vol}`)
+            Nutriname: `${res.data[2].recNutri.nutri_name}`,
+            value: parseInt(`${res.data[2].recNutri.click_vol}`)
         }]
         setRecommend(temp);
         setVisibleChart(true)
-
-           }else if(gender=='w' && age == 60){
-            let temp = [{
-            Nutriname: `${res.data[15].recNutri.nutri_name}`,
-            value: parseInt(`${res.data[15].recNutri.click_vol}`)
-        },{
-            Nutriname: `${res.data[16].recNutri.nutri_name}`,
-            value: parseInt(`${res.data[16].recNutri.click_vol}`)
-        },{
-            Nutriname: `${res.data[17].recNutri.nutri_name}`,
-            value: parseInt(`${res.data[17].recNutri.click_vol}`)
-        }]
-        setRecommend(temp);
-        setVisibleChart(true)
-
-           }else if(gender=='m' && age == 60){
-            let temp = [{
-              Nutriname: `${res.data[18].recNutri.nutri_name}`,
-              value: parseInt(`${res.data[18].recNutri.click_vol}`)
-          },{
-              Nutriname: `${res.data[19].recNutri.nutri_name}`,
-              value: parseInt(`${res.data[19].recNutri.click_vol}`)
-          },{
-              Nutriname: `${res.data[20].recNutri.nutri_name}`,
-              value: parseInt(`${res.data[20].recNutri.click_vol}`)
-          }]
-          setRecommend(temp);
-          setVisibleChart(true)
-
-           }else if(gender=='m' && age == 50){
-            let temp = [{
-              Nutriname: `${res.data[21].recNutri.nutri_name}`,
-              value: parseInt(`${res.data[21].recNutri.click_vol}`)
-          },{
-              Nutriname: `${res.data[22].recNutri.nutri_name}`,
-              value: parseInt(`${res.data[22].recNutri.click_vol}`)
-          },{
-              Nutriname: `${res.data[23].recNutri.nutri_name}`,
-              value: parseInt(`${res.data[23].recNutri.click_vol}`)
-          }]
-          setRecommend(temp);
-          setVisibleChart(true)
-
-           }else if(gender=='m' && age == 40){
-            let temp = [{
-              Nutriname: `${res.data[24].recNutri.nutri_name}`,
-              value: parseInt(`${res.data[24].recNutri.click_vol}`)
-          },{
-              Nutriname: `${res.data[25].recNutri.nutri_name}`,
-              value: parseInt(`${res.data[25].recNutri.click_vol}`)
-          },{
-              Nutriname: `${res.data[26].recNutri.nutri_name}`,
-              value: parseInt(`${res.data[26].recNutri.click_vol}`)
-          }]
-          setRecommend(temp);
-          setVisibleChart(true)
-
-           }else if(gender=='m' && age == 30){
-            let temp = [{
-              Nutriname: `${res.data[27].recNutri.nutri_name}`,
-              value: parseInt(`${res.data[27].recNutri.click_vol}`)
-          },{
-              Nutriname: `${res.data[28].recNutri.nutri_name}`,
-              value: parseInt(`${res.data[28].recNutri.click_vol}`)
-          },{
-              Nutriname: `${res.data[29].recNutri.nutri_name}`,
-              value: parseInt(`${res.data[29].recNutri.click_vol}`)
-          }]
-          setRecommend(temp);
-          setVisibleChart(true)
-
-           }else if(gender=='m' && age == 20){
-            let temp = [{
-              Nutriname: `${res.data[30].recNutri.nutri_name}`,
-              value: parseInt(`${res.data[30].recNutri.click_vol}`)
-          },{
-              Nutriname: `${res.data[31].recNutri.nutri_name}`,
-              value: parseInt(`${res.data[31].recNutri.click_vol}`)
-          },{
-              Nutriname: `${res.data[32].recNutri.nutri_name}`,
-              value: parseInt(`${res.data[32].recNutri.click_vol}`)
-          }]
-          setRecommend(temp);
-          setVisibleChart(true)
-
-           }else{
-            let temp = [{
-              Nutriname: `${res.data[33].recNutri.nutri_name}`,
-              value: parseInt(`${res.data[33].recNutri.click_vol}`)
-          },{
-              Nutriname: `${res.data[34].recNutri.nutri_name}`,
-              value: parseInt(`${res.data[34].recNutri.click_vol}`)
-          },{
-              Nutriname: `${res.data[35].recNutri.nutri_name}`,
-              value: parseInt(`${res.data[35].recNutri.click_vol}`)
-          }]
-          setRecommend(temp);
-          setVisibleChart(true)
-           }
 
        }).catch((e)=>{
            console.log('error', e);
